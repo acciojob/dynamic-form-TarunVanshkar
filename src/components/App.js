0c5b7d295b6bc3bf90b76114bcff6f985a2d659a
TarunVanshkar <tarunben33@gmail.com>
2023-08-23 23:27:23 +0530

    submitting code-Wed Aug 23 2023 23:27:13 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,55 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[AddField, setAddField] = useState([]);
+
+  const handleAddField = () => {
+    const newField = {
+      id: new Date().getTime(),
+      value: "",
+    };
+    setAddField([...AddField, newField]);
+  };
+
+  const deleteField = (id) => {
+    const updatedField = AddField.filter((item) => item.id !== id);
+    setAddField(updatedField);
+  };
+
+  const handleInputChange = (id, value) => {
+    const updatedField = AddField.map((val) => {
+      if (val.id === id) {
+        return { ...AddField, value };
+      }
+      return AddField;
+    });
+    setAddField(updatedField);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+      {AddField.length === 0 ? (
+        <p>No fields in the form</p>
+      ) : (
+        AddField.map((field) => (
+          <div key={field.id}>
+            <input
+              type="text"
+              value={field.value}
+              onChange={(e) => handleInputChange(field.id, e.target.value)}
+            />
+            <button className="delete" onClick={() => deleteField(field.id)}>
+              Delete
+            </button>
+            <br />
+          </div>
+        ))
+      )}
+
+      <button className="add" onClick={handleAddField}>
+        Add Field
+      </button>
     </div>
   )
 }
